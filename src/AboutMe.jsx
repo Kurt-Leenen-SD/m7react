bd5ff1201be6e6d509b0ca5cb8a3dede469a1f36
Kurt-Leenen-SD <39231@ma-web.nl>
2026-02-12 12:17:55 +0100

    ja

---
+++ src/AboutMe.jsx
@@ -0,0 +1,7 @@
+export function AboutMe(text) {
+    return (
+        <>
+            <h2>{text}</h2>
+        </>
+    )
+}
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { AboutMe } from './AboutMe'
+
+function App() {
+  return (
+    <>
+      <main className="main">
+        <h1>Hello World</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt dolorum aut, laborum sit est voluptatem ipsum consequuntur odio recusandae dolore? Neque nemo quaerat vel dolorem incidunt voluptatibus excepturi cum molestias.</p>
+      </main>
+      <AboutMe text={"hello"}/>
+    </>
+  )
+}
+
+export default App
